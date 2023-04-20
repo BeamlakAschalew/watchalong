ef77e7c698a677f5ba7c5873aae8e4197798fe0f
Beamlak Aschalew <birrletej12@gmail.com>
2023-04-20 11:42:06 +0300

    added theme and navigation bar

---
+++ lib/constants/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  static ThemeData themeData() {
+    return ThemeData(
+      appBarTheme: AppBarTheme(color: Color(0xFFE62E04)),
+    );
+  }
+}
